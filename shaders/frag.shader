71ad6e22008fa81954d59894f4b8d0c65884c06a
Mason Stallmo <masonstallmo@gmail.com>
2017-11-13 21:02:50 -0600

    Rotating color interpolated cube

--- shaders/frag.shader
+++ shaders/frag.shader
@@ -1,4 +1,12 @@
 #version 430
+
+in vec4 varyingColor;
 out vec4 color;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
 void main(void)
-{ if (gl_FragCoord.x < 200) color = vec4(1.0, 0.0, 0.0, 1.0); else color = vec4(0.0, 0.0, 1.0, 1.0); }
+{
+    color = varyingColor;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import graphicslib3D.*;
+import graphicslib3D.GLSLUtils.*;
 import java.nio.*;
 import java.io.File;
 import java.io.IOException;
@@ -17,55 +19,122 @@ public class Main extends JFrame implements GLEventListener
     private GLCanvas myCanvas;
     private int rendering_program;
     private int vao[] = new int[1];
-    private float x = 0.0f;
-    private float inc = 0.01f;
+    private int vbo[] = new int[1];
+    private float cameraX, cameraY, cameraZ;
+    private float cubeLocX, cubeLocY, cubeLocZ;
+    private GLSLUtils util = new GLSLUtils();
 
     public Main()
     {
-        setTitle("Chapter 2 - program 1");
+        setTitle("Chapter 4 - program 1a");
         setSize(600, 400);
         setLocation(200, 200);
         myCanvas = new GLCanvas();
         myCanvas.addGLEventListener(this);
-        this.add(myCanvas);
-        setVisible(true);
+        getContentPane().add(myCanvas);
+        this.setVisible(true);
 
-        FPSAnimator animtr = new FPSAnimator(myCanvas, 50);
-        animtr.start();
+        FPSAnimator animator = new FPSAnimator(myCanvas, 30);
+        animator.start();
     }
 
     public void display (GLAutoDrawable drawable)
     {
         GL4 gl = (GL4) GLContext.getCurrentGL();
-        gl.glUseProgram(rendering_program);
-
+        gl.glClear(GL_DEPTH_BUFFER_BIT);
         float bkg[] = { 0.0f, 0.0f, 0.0f, 1.0f };
         FloatBuffer bkgBuffer = Buffers.newDirectFloatBuffer(bkg);
         gl.glClearBufferfv(GL_COLOR, 0, bkgBuffer);
-        x += inc;
-        if (x > 1.0f)
-            inc = -0.01f;
-        if (x < -1.0f)
-            inc = 0.01f;
-        int offsset_loc = gl.glGetUniformLocation(rendering_program, "offset");
-        gl.glProgramUniform1f(rendering_program, offsset_loc, x);
+        gl.glUseProgram(rendering_program);
 
-        gl.glDrawArrays(GL_TRIANGLES, 0, 3 );
+        int mv_loc = gl.glGetUniformLocation(rendering_program, "mv_matrix");
+        int proj_loc = gl.glGetUniformLocation(rendering_program, "proj_matrix");
+
+        float aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
+        Matrix3D pMat = perspective(60.0f, aspect, 0.1f,  1000.0f);
+
+        Matrix3D vMat = new Matrix3D();
+        vMat.translate(-cameraX, -cameraY, -cameraZ);
+
+        Matrix3D mMat = new Matrix3D();
+        double t = (double)(System.currentTimeMillis()) / 1000.0;
+        mMat.translate(Math.sin(2*t)*2.0, Math.sin(3*t)*2.0, Math.sin(4*t)*2.0);
+        mMat.rotate(100*t, 100*t, 100*t);
+
+        Matrix3D mvMat = new Matrix3D();
+        mvMat.concatenate(vMat);
+        mvMat.concatenate(mMat);
+
+        gl.glUniformMatrix4fv(mv_loc, 1, false, mvMat.getFloatValues(), 0);
+        gl.glUniformMatrix4fv(proj_loc, 1, false, pMat.getFloatValues(), 0);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        gl.glEnableVertexAttribArray(0);
+
+        gl.glEnable(GL_DEPTH_TEST);
+        gl.glDepthFunc(GL_LEQUAL);
+
+        gl.glDrawArrays(GL_TRIANGLES, 0, 36 );
+    }
+
+    public void init(GLAutoDrawable drawable)
+    {
+        GL4 gl = (GL4) drawable.getGL();
+        rendering_program = createShaderProgram();
+        setupVertices();
+        cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+        cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    }
+
+
+    private void setupVertices()
+    {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        float[] vertex_positions =
+        {-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+         -1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+         -1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+         -1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+         -1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+         -1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+        };
+
+        gl.glGenVertexArrays(vao.length, vao, 0);
+        gl.glBindVertexArray(vao[0]);
+        gl.glGenBuffers(vbo.length, vbo, 0);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(vertex_positions);
+        gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
+    }
+
+    private Matrix3D perspective(float fovy, float aspect, float n, float f)
+    {
+        float q = 1.0f / ((float) Math.tan(Math.toRadians(0.5f * fovy)));
+        float A = q / aspect;
+        float B = (n + f) / (n - f);
+        float C = (2.0f * n * f) / (n - f);
+        Matrix3D r = new Matrix3D();
+        r.setElementAt(0,0,A);
+        r.setElementAt(1,1,q);
+        r.setElementAt(2,2,B);
+        r.setElementAt(3,2,-1.0f);
+        r.setElementAt(2,3,C);
+        r.setElementAt(3,3,0.0f);
+        return r;
     }
 
     public static void main(String[] args)
     {
         new Main();
     }
-
-    public void init(GLAutoDrawable drawable)
-    {
-        GL4 gl = (GL4) GLContext.getCurrentGL();
-        rendering_program = createShaderProgram();
-        gl.glGenVertexArrays(vao.length, vao, 0);
-        gl.glBindVertexArray(vao[0]);
-    }
-
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
     public void dispose(GLAutoDrawable drawable) {}
 
@@ -222,5 +291,7 @@ public class Main extends JFrame implements GLEventListener
         }
         return foundError;
     }
+
+
 
 }
